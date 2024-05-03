a554d7c56fec5fe9e759ccc9b6b4906addcbfc9d
Luis Reyes <luisandresreyes@hotmail.com>
2024-05-03 16:56:14 -0300

    Nuevo orden para los usuarios - Inicio

--- src/app/layouts/dashboard/pages/alumnos/services/alumnos.service.ts
+++ src/app/layouts/dashboard/pages/alumnos/services/alumnos.service.ts
@@ -8,41 +8,7 @@ import { IAlumno } from '../models';
 })
 export class AlumnosService {
     private alumnos: IAlumno[] = [
-        {
-          id: 1,
-          nombre: 'Juan',
-          apellido: 'Pérez',
-          edad: 28,
-          correo: 'juanperez@example.com',
-        },
-        {
-          id: 2,
-          nombre: 'María',
-          apellido: 'Gómez',
-          edad: 25,
-          correo: 'mariagomez@example.com',
-        },
-        {
-          id: 3,
-          nombre: 'Carlos',
-          apellido: 'Sánchez',
-          edad: 30,
-          correo: 'carlossanchez@example.com',
-        },
-        {
-          id: 4,
-          nombre: 'Laura',
-          apellido: 'Rodríguez',
-          edad: 22,
-          correo: 'laurarodriguez@example.com',
-        },
-        {
-          id: 5,
-          nombre: 'Pedro',
-          apellido: 'Martínez',
-          edad: 24,
-          correo: 'pedromartinez@example.com',
-        },
+        
       ];
     
   
--- src/app/layouts/dashboard/pages/usuarios/services/usuarios.service.ts
+++ src/app/layouts/dashboard/pages/usuarios/services/usuarios.service.ts
@@ -8,24 +8,7 @@ import { IUsuario } from '../models';
 })
 export class UsuariosService {
   private usuarios: IUsuario[] = [
-    {
-      id: 1,
-      usuario: 'admin',
-      password: 'pass',
-      email: 'correo1@mail.com',
-      rol: 'ADMIN',
-      fecha_creacion: new Date('09/08/2021'),
-      fecha_modificacion: new Date('09/08/2021'),
-    },
-    {
-      id: 2,
-      usuario: 'user',
-      password: 'pass',
-      email: 'correo2@mail.com',
-      rol: 'USER',
-      fecha_creacion: new Date('09/08/2021'),
-      fecha_modificacion: new Date('09/08/2021'),
-    },
+    
   ];
   
   constructor() {}
